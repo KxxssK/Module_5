d4fad08838fd8a73b46e1454c73a1ba6739d1474
KxxssK <Kxxssk@yandex.ru>
2024-10-06 13:47:37 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -1,10 +1,4 @@
 class House:
-    houses_history = []
-
-    def __new__(cls, *args, **kwargs):
-        cls.houses_history.append(args[0])
-        return super().__new__(cls)
-
 
     def __init__(self, name, number_of_floors):
         self.name = name
@@ -63,19 +57,28 @@ class House:
             self.number_of_floors += value
         return self
 
-    def __del__(self):
-        print(self.name, 'снесен, но останется в истории')
 
 
 h1 = House('ЖК Эльбрус', 10)
-print(House.houses_history)
 h2 = House('ЖК Акация', 20)
-print(House.houses_history)
-h3 = House('ЖК Матрёшки', 20)
-print(House.houses_history)
 
-# Удаление объектов
-del h2
-del h3
+print(h1)
+print(h2)
 
-print(House.houses_history)
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
